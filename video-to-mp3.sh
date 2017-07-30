d9d6c8137a03c5f5265b34352eeba1752c80fab4
oleks <oleks@oleks.info>
2017-07-30 14:49:52 +0200

    Mention the license in the source code itself

--- video-to-mp3.sh
+++ video-to-mp3.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Copyright 2017 oleks <oleks@oleks.info>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. A copy of the License
+# text is included alongside this file as LICENSE.md.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
 set -euo pipefail
 
 path=$1
